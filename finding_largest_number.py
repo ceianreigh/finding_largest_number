7e480f03a2afef3fc67083713d65164ee6085e29
Ceianreigh Keann Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-01-06 16:21:55 +0800

    Adding the instruction and pseudocode

---
+++ finding_largest_number.py
@@ -0,0 +1,8 @@
+# Assignment 4
+# Ask user to input 3 numbers. Find and print the biggest number using only if statements.
+
+# pseudocode
+
+# Ask user to input 3 numbers
+# Finding the biggest number using if statements
+# Print the biggest number
