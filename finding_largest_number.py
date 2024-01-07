7c4192e62c8008b9cfbec4447ddbb55c6914065f
Ceianreigh Keann Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-01-07 13:17:38 +0800

    Updated window geometry and added find button

--- finding_largest_number.py
+++ finding_largest_number.py
@@ -7,7 +7,7 @@ from tkinter import PhotoImage, messagebox
 # pseudocode
 window = tk.Tk()
 window.title("Find the Biggest Number")
-window.geometry("500x400")
+window.geometry("500x250")
 
 # heading
 heading = tk.Label(
@@ -48,6 +48,15 @@ number_3_label = tk.Label(numbers_frame, text="Enter Number 3:")
 number_3_label.grid(row=0, column=2, padx=10, pady=5)
 number_3_entry = tk.Entry(numbers_frame)
 number_3_entry.grid(row=1, column=2, padx=10, pady=5)
+
+# find button
+find_button = tk.Button(
+    frame,
+    text="Find",
+    font=("Arial", 10),
+    command=lambda: find_biggest_number(),
+)
+find_button.grid(row=1, column=0, padx=10, pady=10, columnspan=3, sticky=tk.W + tk.E)
 
 # ask user to input 3 numbers
 number_1 = int(input("Enter number 1: "))
